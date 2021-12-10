b022f70b00463a9a0532eee1c58f724fdb113bdc
Artmeiy <art@gmail.com>
2021-12-10 12:44:21 +0300

    Added navigation buttons

--- app/src/main/java/com/example/todolist_example/ui/HomeFragment.java
+++ app/src/main/java/com/example/todolist_example/ui/HomeFragment.java
@@ -1,21 +1,48 @@
 package com.example.todolist_example.ui;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+
 import com.example.todolist_example.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class HomeFragment extends Fragment {
 
+    FloatingActionButton addTaskButton;
+
+    private FragmentActivity activityContext;
+
+    @Override
+    public void onAttach(@NonNull Activity activity) {
+        activityContext = (FragmentActivity) activity;
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        addTaskButton = view.findViewById(R.id.add_task_button);
+        addTaskButton.setOnClickListener(v -> {
+            activityContext.getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.nav_host_fragment_main, NewTaskFragment.class, null)
+                    .commit();
+        });
+
+        return view;
     }
 }
